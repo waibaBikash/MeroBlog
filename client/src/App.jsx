254dd9000ae15be82467538df8799adc29f676ef
waibaBikash <kavrelibikash@gmail.com>
2025-06-02 14:51:41 +1000

    feat: implement routing with Home and Blog pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
     <div>
-      
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+       </Routes>
     </div>
   )
 }
