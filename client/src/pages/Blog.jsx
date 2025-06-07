e0abae5eef8c36e52fcb9a31f790a1ad0159eadc
waibaBikash <kavrelibikash@gmail.com>
2025-06-07 20:36:46 +1000

    feat: add comments section to Blog component with functionality to display and add comments

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
-import { assets, blog_data } from '../assets/assets';
+import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment'
 
@@ -9,14 +9,27 @@ const Blog = () => {
   const {id} = useParams();
 
   const [data, setData] = useState(null);
+  const [comments, setComments] = useState([])
+
+  const [name, setName] = useState('')
+  const [content, setContent] = useState('')
 
   const fetchBlogData = async ()=> {
    const data = blog_data.find(item => item._id === id)
    setData(data)
   }
 
+  const fetchComments = async () => {
+    setComments(comments_data);
+  }
+
+  const addComment = async (e)=>{
+   e.preventDefault();
+  }
+
   useEffect(()=>{
-fetchBlogData()
+     fetchBlogData()
+     fetchComments()
   },[])
   return data ?  (
     <div className='relative'>
@@ -31,7 +44,41 @@ fetchBlogData()
         <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 font-medium text-primary'>Michael Brown</p>
       </div>
 
-      <div></div>
+      <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+         <img src={data.image} alt="" className='rounded-3xl mb-5' />
+           
+           <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}>
+           </div>
+
+           {/* Comments Section  */}
+
+           <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+              <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+                <div className='flex flex-col gap-4'>
+                  {comments.map((item, index)=>(
+                    <div className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600' key={index}>
+                       <div className='flex items-center g2 mb-2'>
+                         <img src={assets.user_icon} alt="" className='w-6' />
+                         <p className='font-medium'>{item.name}</p>
+                       </div>
+                        <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                        <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+                    </div>
+                  ))}
+                </div>
+           </div>
+            {/* Add Comment Section  */}
+           <div className='max-w-3xl mx-auto'>
+             <p className='font-semibold mb-4'>Add your comment</p>
+             <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+               <input onChange={(e)=> setName(e.target.value)} value={name} className='w-full p-2 border border-gray-300 rounded outline-none' type="text" placeholder='Name' required />
+
+               <textarea onChange={(e)=> setContent(e.target.value)} value={content} placeholder='Comment' className='w-full p-2 border border-gray-300 rounded outline-none h-48'></textarea>
+
+               <button className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer' type='submit'>Submit</button>
+             </form>
+           </div>
+      </div>
     </div>
   ) : <div>Loading...</div>
 }
