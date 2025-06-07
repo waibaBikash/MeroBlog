605fbe409c6bc535b6f3c30d81a0620ca7d2ef34
waibaBikash <kavrelibikash@gmail.com>
2025-06-07 20:46:54 +1000

    style: improve code formatting and readability in Blog component

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -34,21 +34,34 @@ const Blog = () => {
   },[])
   return data ?  (
     <div className='relative'>
-       <img src={assets.gradientBackground} alt="" className='absolute -top-0.5 -z-1 opacity-50' />
+       <img 
+       src={assets.gradientBackground}
+       alt="" className='absolute -top-0.5 -z-1 opacity-50'
+       />
 
       <Navbar />
 
       <div className='text-center mt-20 text-shadow-gray-600'>
-        <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMM Do YYYY')}</p>
-        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
-        <h2 className='my-5 max-w-lg truncate mx-auto '>{data.subTitle}</h2>
-        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 font-medium text-primary'>Michael Brown</p>
+        <p className='text-primary py-4 font-medium'>
+         Published on {Moment(data.createdAt).format('MMM Do YYYY')}
+         </p>
+        <h1 
+        className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>
+          {data.title}
+          </h1>
+        <h2 
+        className='my-5 max-w-lg truncate mx-auto '>{data.subTitle}</h2>
+        <p 
+        className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 font-medium text-primary'>
+          Michael Brown
+        </p>
       </div>
 
       <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
          <img src={data.image} alt="" className='rounded-3xl mb-5' />
            
-           <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}>
+           <div
+            className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}>
            </div>
 
            {/* Comments Section  */}
@@ -63,7 +76,9 @@ const Blog = () => {
                          <p className='font-medium'>{item.name}</p>
                        </div>
                         <p className='text-sm max-w-md ml-8'>{item.content}</p>
-                        <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+                        <div 
+                        className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>
+                          {Moment(item.createdAt).fromNow()}</div>
                     </div>
                   ))}
                 </div>
@@ -72,11 +87,24 @@ const Blog = () => {
            <div className='max-w-3xl mx-auto'>
              <p className='font-semibold mb-4'>Add your comment</p>
              <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
-               <input onChange={(e)=> setName(e.target.value)} value={name} className='w-full p-2 border border-gray-300 rounded outline-none' type="text" placeholder='Name' required />
+               <input 
+               onChange={(e)=> setName(e.target.value)} 
+               value={name} 
+               className='w-full p-2 border border-gray-300 rounded outline-none'
+                type="text"
+                 placeholder='Name'
+                  required />
 
-               <textarea onChange={(e)=> setContent(e.target.value)} value={content} placeholder='Comment' className='w-full p-2 border border-gray-300 rounded outline-none h-48'></textarea>
+               <textarea
+                onChange={(e)=> setContent(e.target.value)} 
+                value={content} placeholder='Comment'
+                 className='w-full p-2 border border-gray-300 rounded outline-none h-48'>
+                 </textarea>
 
-               <button className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer' type='submit'>Submit</button>
+               <button
+                className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer' 
+                type='submit'>Submit
+                </button>
              </form>
            </div>
              <div className='my-24 max-w-3xl mx-auto'>
