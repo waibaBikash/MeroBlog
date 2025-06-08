2e888d37e82c7651127215297be7077409e2d2a8
waibaBikash <kavrelibikash@gmail.com>
2025-06-08 18:52:13 +1000

    feat: implement layout structure with navigation and logout functionality

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {assets} from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Layout = () => {
+
+  const navigate = useNavigate();
+
+  const logout = ()=> {
+    navigate('/')
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+         <img onClick={()=> navigate('/')}
+         src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' />
+         <button onClick={logout}
+          className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
+      </div>
+    </>
   )
 }
 
