b38d0edf1f7f144cfa487867bac62d143ff6adea
waibaBikash <kavrelibikash@gmail.com>
2025-06-08 19:51:52 +1000

    fix: improve formatting and readability in Login component

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -13,7 +13,9 @@ const Login = () => {
       <div className='w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg'>
          <div className='flex flex-col items-center justify-center'>
              <div className='w-full py-6 text-center'>
-               <h1 className='text-3xl font-bold'> <span className='text-primary'> Admin</span> Login</h1>
+               <h1 className='text-3xl font-bold'>
+                 <span className='text-primary'> Admin</span> 
+                 Login</h1>
                <p className='font-light'>Enter your credentials to acces the amdin panel</p>
              </div>
               <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
@@ -34,7 +36,9 @@ const Login = () => {
                        className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                   </div>
                   <button type='submit'
-                   className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'>Login</button>
+                   className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'>
+                    Login
+                    </button>
               </form>
          </div>
       </div>
