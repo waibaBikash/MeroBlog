5ac85c59841e0f3dc8c1e07b2d5d5d7516d08b50
waibaBikash <kavrelibikash@gmail.com>
2025-06-08 19:10:15 +1000

    feat: add Sidebar component for admin navigation and integrate it into Layout

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {assets} from '../../assets/assets'
-import { useNavigate } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Sidebar from '../../components/admin/Sidebar';
 
 const Layout = () => {
 
@@ -17,6 +18,10 @@ const Layout = () => {
          src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' />
          <button onClick={logout}
           className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
+      </div>
+      <div className='flex h-[calc(100vh-70px)]'>
+            <Sidebar/>
+            <Outlet/>
       </div>
     </>
   )
