62e44265f23d73dbef25ee5d0fac5a2caddc8979
waibaBikash <kavrelibikash@gmail.com>
2025-06-08 19:50:05 +1000

    fix: clean up formatting and ensure consistent use of semicolons in Dashboard component

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { assets, dashboard_data } from '../../assets/assets'
+import React, { useEffect, useState } from 'react';
+import { assets, dashboard_data } from '../../assets/assets';
 
 const Dashboard = () => {
 
@@ -22,7 +22,8 @@ const Dashboard = () => {
     <div className='flex-1 p-4 md:p-10 bg-blue-50/50'>
        <div className='flex flex-wrap gap-4'>
 
-           <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+           <div 
+           className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
              <img src={assets.dashboard_icon_1} alt="" />
                <div >
                  <p className='text-xl font-semibold text-gray-600'>{dasboardData.blogs}</p>
@@ -30,7 +31,8 @@ const Dashboard = () => {
                </div>
            </div>
 
-           <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+           <div 
+           className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
              <img src={assets.dashboard_icon_2} alt="" />
                <div >
                  <p className='text-xl font-semibold text-gray-600'>{dasboardData.comments}</p>
@@ -51,4 +53,4 @@ const Dashboard = () => {
   )
 }
 
-export default Dashboard
+export default Dashboard;
