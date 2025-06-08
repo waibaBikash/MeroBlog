132dd2d2199b8a730b4f4c606b12685b6698e3cd
waibaBikash <kavrelibikash@gmail.com>
2025-06-08 19:51:01 +1000

    fix: improve formatting and consistency in Login component inputs

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -19,13 +19,22 @@ const Login = () => {
               <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
                   <div className='flex flex-col'>
                      <label>Email</label>
-                     <input onChange={e=> setEmail(e.target.value)} value={email} type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                     <input 
+                     onChange={e=> setEmail(e.target.value)} 
+                     value={email} type="email" required 
+                     placeholder='your email id' 
+                     className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                   </div>
                   <div className='flex flex-col'>
                      <label>Password</label>
-                     <input onChange={e=> setPassword(e.target.value)} value={password} type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                     <input 
+                     onChange={e=> setPassword(e.target.value)} 
+                     value={password} type="password" required
+                      placeholder='your password'
+                       className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                   </div>
-                  <button type='submit' className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'>Login</button>
+                  <button type='submit'
+                   className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'>Login</button>
               </form>
          </div>
       </div>
@@ -33,4 +42,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
