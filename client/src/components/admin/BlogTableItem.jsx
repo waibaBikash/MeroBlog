f61c9b76ca008723b85dd6284fc9d5ec7cb13af0
waibaBikash <kavrelibikash@gmail.com>
2025-06-09 20:48:03 +1000

    fix: correct variable name and add BlogTableItem component to Dashboard

---
+++ client/src/components/admin/BlogTableItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../../assets/assets';
+
+const BlogTableItem = ({blog, fetchBlogs, index}) => {
+  const {title, createdAt} = blog;
+  const BlogDate = new Date(createdAt)
+  return (
+    <tr className='border-y border-gray-300'>
+       <th className='px-2 py-4'>{ index }</th>
+       <td className='px-2 py-4'>{title}</td>
+       <td className='px-2 py-4 max-sm:hidden'>{BlogDate.toDateString()}</td>
+       <td className='px-2 py-4 max-sm:hidden'>
+        <p className={`${blog.isPublished ? 'bg-green-800' : 'text-orange-700'}`}>{blog.isPublished ? 'Published' : 'Unpublished'}</p>
+       </td>
+       <td className='px-2 py-4 flex text-sm gap-3'>
+         <button className='border px-2 py-0.5 rounded cursor-pointer'>{blog.isPublished ? 'Unpublish' : 'Publish'}</button>
+          <img src={assets.cross_icon} alt=""  className='w-8 hover:scale-110 transition-all cursor-pointer'/>
+       </td>
+    </tr>
+  )
+}
+
+export default BlogTableItem
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { assets, dashboard_data } from '../../assets/assets';
+import BlogTableItem from '../../components/admin/BlogTableItem';
 
 const Dashboard = () => {
 
-  const [dasboardData, setDashboardData] = useState({
+  const [dashboardData, setDashboardData] = useState({
     blogs:0,
     comments: 0,
     drafts: 0,
@@ -26,7 +27,7 @@ const Dashboard = () => {
            className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
              <img src={assets.dashboard_icon_1} alt="" />
                <div >
-                 <p className='text-xl font-semibold text-gray-600'>{dasboardData.blogs}</p>
+                 <p className='text-xl font-semibold text-gray-600'>{dashboardData.blogs}</p>
                  <p className='text-gray-400 font-light'>Blogs</p>
                </div>
            </div>
@@ -35,7 +36,7 @@ const Dashboard = () => {
            className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
              <img src={assets.dashboard_icon_2} alt="" />
                <div >
-                 <p className='text-xl font-semibold text-gray-600'>{dasboardData.comments}</p>
+                 <p className='text-xl font-semibold text-gray-600'>{dashboardData.comments}</p>
                  <p className='text-gray-400 font-light'>Comments</p>
                </div>
            </div>
@@ -43,11 +44,40 @@ const Dashboard = () => {
            <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
              <img src={assets.dashboard_icon_3} alt="" />
                <div >
-                 <p className='text-xl font-semibold text-gray-600'>{dasboardData.drafts}</p>
+                 <p className='text-xl font-semibold text-gray-600'>{dashboardData.drafts}</p>
                  <p className='text-gray-400 font-light'>Drafts</p>
                </div>
            </div>
        </div>
+        <div>
+           <div className='flex items-center gap-3 mt-6 text-gray-600'>
+             <img src={assets.dashboard_icon_4} alt="" />
+             <p>Latest Blogs</p>
+           </div>
+             <div className='relative max-w-4xl overflow-auto shadow rounded-lg'>
+                <table className='w-full text-sm text-gray-500'>
+                   <thead className='text-xs text-gray-600 text-left uppercase'>
+                       <tr>
+                          <th scope='col' className='px-2 py-4 xl:px-6'>#</th>
+                          <th scope='col' className='px-2 py-4'>Blog Title</th>
+                          <th scope='col' className='px-2 py-4 max-sm:hidden'>Date</th>
+                          <th scope='col' className='px-2 py-4 max-sm:hidden'>Status</th>
+                          <th scope='col' className='px-2 py-4'>Actions</th>
+                       </tr>
+                   </thead>
+                    <tbody>
+                       {
+                        dashboardData.recentBlog.map((blog, index)=>{
+                          return <BlogTableItem 
+                          key={blog._id} fetchBlogs={fetchDashboard}
+                          index={index + 1}
+                           />
+                        })
+                       }
+                    </tbody>
+                </table>
+             </div>
+        </div>
       
     </div>
   )
