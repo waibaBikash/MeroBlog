4bdbe3c8290df6675dd6075b9689e63034f9a065
waibaBikash <kavrelibikash@gmail.com>
2025-06-16 20:23:29 +1000

    fix: improve formatting of BlogTableItem component for better readability

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -3,17 +3,21 @@ import { assets } from '../../assets/assets'
 
 const BlogTableItem = ({blog, fetchBlogs, index}) => {
 
-  const { title, createdAt } = blog
+  const { title, createdAt } = blog;
   
-    const BlogDate = new Date(createdAt)
+    const BlogDate = new Date(createdAt);
 
   return (
     <tr className='border-y border-gray-300'>
         <th className='px-2 py-4'>{ index } </th>
         <td className='px-2 py-4'>{title}</td>
-        <td className='px-2 py-4 max-sm:hidden'> {BlogDate.toDateString()} </td>
         <td className='px-2 py-4 max-sm:hidden'>
-          <p className={`${blog.isPublished ? "text-green-600" : "text-orange-700"}`}>{blog.isPublished ? 'Published' : 'Unpublished'}</p>
+         {BlogDate.toDateString()} </td>
+        <td className='px-2 py-4 max-sm:hidden'>
+          <p 
+          className={`${blog.isPublished ? "text-green-600" : "text-orange-700"}`}>
+            {blog.isPublished ? 'Published' : 'Unpublished'}
+            </p>
         </td>
         <td className='px-2 py-4 flex text-xs gap-3'>
              <button 
