bc192eba9dbd544e74ce70de8ea161e93811d15a
waibaBikash <kavrelibikash@gmail.com>
2025-06-16 20:22:33 +1000

    fix: improve formatting of BlogTableItem component for better readability

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -16,7 +16,10 @@ const BlogTableItem = ({blog, fetchBlogs, index}) => {
           <p className={`${blog.isPublished ? "text-green-600" : "text-orange-700"}`}>{blog.isPublished ? 'Published' : 'Unpublished'}</p>
         </td>
         <td className='px-2 py-4 flex text-xs gap-3'>
-             <button className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>{blog.isPublished ? 'Unpublished' : 'Published'}</button>
+             <button 
+             className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>
+              {blog.isPublished ? 'Unpublished' : 'Published'}
+              </button>
              <img src={assets.cross_icon} alt="" 
              className='w-8 hover:scale-110 transition-all cursor-pointer' />
         </td>
