f0fa3b65462b203f59649781f1d2331a91e33535
waibaBikash <kavrelibikash@gmail.com>
2025-06-19 13:19:05 +1000

    fix: refactor AddBlog component to manage state and handle form submission

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
 
 const AddBlog = () => {
+
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState('');
+  const [subTitle, setSubTitle] = useState('');
+  const [category, setCategory] = useState('Startup');
+  const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmmitHandler = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form  onSubmit={onSubmmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+       <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
+         <p>Upload thumpnail</p>
+         <label htmlFor="image">
+           <img src={! image ? assets.upload_area : URL.createObjectURL(image)} alt="" className='mt-2 h-1/6 rounded cursor-pointer' />
+            <input onChange={(e)=> setImage(e.target.files[0])} type="file"  id='image' hidden required/>
+         </label>
+
+         <p  className='mt-4'>Blog title</p>
+         <input type="text" placeholder='Type here' required className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded ' onChange={(e)=> setTitle(e.target.value)} value={title}/>
+       </div>
+    </form>
   )
 }
 
