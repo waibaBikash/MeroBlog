b02fe2f6c3f2ff04db3978053087b717f3683cfe
waibaBikash <kavrelibikash@gmail.com>
2025-06-20 13:45:38 +1000

    feat: add blog category selection and publish option in AddBlog component

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,5 +1,5 @@
 import React, { use, useEffect, useRef, useState } from 'react'
-import { assets } from '../../assets/assets'
+import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
 
 const AddBlog = () => {
@@ -49,6 +49,20 @@ const AddBlog = () => {
             <div ref={editorRef}></div>
               <button onClick={generateContent} type='button' className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'>Generate with AI</button>
            </div>
+
+           <p className='mt-4'>Blog Category</p>
+           <select onChange={e => setCategory(e.target.value)} name="category" className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'>
+             <option value="">Select category</option>
+             {blogCategories.map((item, index)=>{
+              return <option key={index} value={item}>{item}</option>
+             })}
+           </select>
+
+            <div className='flex gap-2 mt-4'>
+              <p>Publish Now</p>
+              <input type="checkbox" checked={isPublished} className='scale-125 cursor-pointer' onChange={e => setIsPublished(e.target.checked)} />
+            </div>
+              <button type='submit' className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button>
        </div>
     </form>
   )
