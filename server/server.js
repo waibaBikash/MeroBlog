97127f3b2e39524b0210c0f6c646c63461e7f5d6
waibaBikash <kavrelibikash@gmail.com>
2025-06-23 21:42:28 +1000

    Initialize server with Express, add dependencies, and set up basic API endpoint

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = exrpess();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => (
+  "API is Working"
+))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
