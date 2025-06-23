2b13d85fe4bbef90ffb3a74b0c7f83d1745053b0
waibaBikash <kavrelibikash@gmail.com>
2025-06-23 21:50:27 +1000

    fix: add comments for clarity and improve logging format in server file

--- server/server.js
+++ server/server.js
@@ -8,14 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => (
+// Routes
+
+app.get('/', (req, res) => res.send(
   "API is Working"
 ))
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server is running on port " +PORT);
 });
 
 export default app;
