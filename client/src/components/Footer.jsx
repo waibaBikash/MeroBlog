170b0f290b74be80ef692688a2ba0ba98d5d178e
waibaBikash <kavrelibikash@gmail.com>
2025-07-01 13:37:33 +1000

    fix: improve formatting and readability in Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {assets, footer_data} from '../assets/assets'
+import {assets, footer_data} from '../assets/assets';
 
 const Footer = () => {
   return (
@@ -7,7 +7,8 @@ const Footer = () => {
        <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'>
           <div>
              <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
-             <p className='max-w-[410px] mt-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga voluptatem veniam earum eveniet quia dolorem fugiat rerum numquam ex aliquam?</p>
+             <p className='max-w-[410px] mt-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Fuga voluptatem veniam earum eveniet quia dolorem fugiat rerum numquam ex aliquam?</p>
           </div>
 
             <div className='flex flex-wrap justify-between w-full md:w-[45%] gap-5'>
@@ -27,9 +28,12 @@ const Footer = () => {
                 }
             </div>
        </div>
-       <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuickBlog - All Right Reserved.  </p>
+       <p 
+       className='py-4 text-center text-sm md:text-base text-gray-500/80'>
+        Copyright 2025 © QuickBlog - All Right Reserved.
+        </p>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
